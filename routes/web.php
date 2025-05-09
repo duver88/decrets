e7b26791c4968b2819818df71e927bfd01dacb67
duver <duver20000@gmail.com>
2025-05-09 10:56:52 -0500

    tt

--- routes/web.php
+++ routes/web.php
@@ -42,4 +42,4 @@ Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
     Route::middleware('guest')->group(function () {
         Route::get('/login', [AuthenticatedSessionController::class, 'create'])->name('login');
         Route::post('/login', [AuthenticatedSessionController::class, 'store']);
-    });
+    }); 
